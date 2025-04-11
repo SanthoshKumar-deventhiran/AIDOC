1b0be236fc38531311a1dd64ea68dd08d5ce1427
Santhosh Kumar <133501697+SanthoshKumar-deventhiran@users.noreply.github.com>
2025-04-11 07:47:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, jsonify
 import numpy as np
 import pandas as pd
 import pickle
+import os
 
 # flask app
 app = Flask(__name__)
@@ -112,7 +113,6 @@ def contact():
 @app.route('/blog')
 def blog():
     return render_template("blog.html")
-
 if __name__ == '__main__':
-
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
